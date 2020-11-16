a6b8c453cc07176cc46e6617f2f2bea73624a313
Marcel Just <marcel.just@stud.uni-due.de>
2020-11-16 21:54:30 +0100

    sql voids

--- SEPServer/src/main/java/SEPServer/SQL/SQL.java
+++ SEPServer/src/main/java/SEPServer/SQL/SQL.java
@@ -9,6 +9,8 @@ import java.sql.Statement;
 import java.util.Vector;
 
 import SEPCommon.Response;
+import SEPCommon.User;
+import SEPCommon.Product;
 
 import java.sql.Date;
 
@@ -21,62 +23,62 @@ public class SQL{
 	}
 
 	public Response registerUser(User user) {
-
+		return Response.Success;
 	}
 
 	public Response loginUser(User user) {
-
+		return Response.Success;
 	}
 
 	public Response editUser(User user) {
-
+		return Response.Success;
 	}
 
 	public Response deleteUser(User user) {
-
+		return Response.Success;
 	}
 
 	public Response increaseWallet(User user, double amount) {
-
+		return Response.Success;
 	}
 
 	public Response decreaseWallet(User user, double amount) {
-
+		return Response.Success;
 	}
 
 	public Response fetchProducts() {
 		return Response.Success;
 	}
 
-	public Response fetchProducts(String category) {
-
+	public Response fetchProductsByCategory(String category) {
+		return Response.Success;
 	}
 
-	public Response fetchProducts(String searchString) {
-
+	public Response fetchProductsByString(String searchString) {
+		return Response.Success;
 	}
 
 	public Response fetchLastViewedProducts(User user) {
-
+		return Response.Success;
 	}
 
 	public Response addItem(User seller, Product product) {
-
+		return Response.Success;
 	}
 
 	public Response addItem(User seller, Product[] products) {
-
+		return Response.Success;
 	}
 
 	public Response buyItem(User buyer, Product product) {
-
+		return Response.Success;
 	}
 
 	public User getUserDataByEmail(String email) {
-
+		//
 	}
 
 	public User getUserDataByUsername(String username) {
-
+		//
 	}
 }
