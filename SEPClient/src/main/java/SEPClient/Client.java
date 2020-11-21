d849d58d5cf8577fecadf942659b3c75e3a17711
denis artjuch <denis.artjuch@stud.uni-due.de>
2020-11-21 22:58:49 +0100

    Client

--- SEPClient/src/main/java/SEPClient/Client.java
+++ SEPClient/src/main/java/SEPClient/Client.java
@@ -1,35 +1,78 @@
 package SEPClient;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import SEPCommon.ClientRequest;
+import SEPCommon.Response;
+import SEPCommon.ServerResponse;
 
 public class Client {
 	private Socket clientSocket;
-	private DataInputStream dis; // Daten empfangen
-	private DataOutputStream dos; // Daten schicken
+	private ObjectInputStream dis; // Daten empfangen
+	private ObjectOutputStream dos; // Daten schicken
+	public boolean startSuccess = false;
+	private static Client client; 
 	public boolean isStarted = false;
+
+	public static Client getClient()
+	{
+		return client;
+	}
 	
-	public boolean start() {
+	public void start() {
 		// Socket-Verbindung zum Server herstellen
+		client=this;
 		try {
 			// Client Socket erstellen
 			clientSocket = new Socket(SEPCommon.Constants.SERVERIP, SEPCommon.Constants.PORT);
-			System.out.println("Verbindung zum Server hergestellt");
-			return true;
+			clientSocket.setSoTimeout(10000);
+			System.out.println("Verbindung zum Server hergestellt.");
+			
+            dos = new ObjectOutputStream(clientSocket.getOutputStream());
+            dis = new ObjectInputStream(clientSocket.getInputStream());
+            
+            isStarted=true;
+            
 		} catch (IOException e) {
+			startSuccess=false;
 			e.printStackTrace();
-			return false;
+			isStarted=false;
 		}
 	}
-
-	public void sendClientRequest() {
-
-	}
-
-	public void handleServerRequest() {
-
+	
+	//@SuppressWarnings("unused")
+	@SuppressWarnings("unused")
+	public ServerResponse sendClientRequest(ClientRequest req)
+	{
+		ServerResponse serverResponse = null;
+		try {
+			System.out.println("Sende ClientRequest - " + req.getRequestType() + " - " + req.getRequestMap());
+			
+			//Anfrage senden
+			dos.writeObject(req);
+			
+			//Antwort auslesen
+			serverResponse = (ServerResponse)dis.readObject();
+			System.out.println("ServerResponse - " + serverResponse.getResponseType() + " - " + serverResponse.getResponseMap());
+			
+			if(serverResponse==null)
+			{
+				return new ServerResponse(Response.Failure, null);
+			}
+			else
+			{
+				ServerResponse serverres = (ServerResponse)serverResponse;
+				return serverres;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new ServerResponse(Response.Failure, null);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return new ServerResponse(Response.Failure, null);
+		}
 	}
 
 	public void stop() {
