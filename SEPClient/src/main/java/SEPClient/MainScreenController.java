f41ea393da95fb9703bdfeab33d1bc119c371e77
Kamil Chahrour <kamil.chahrour@stud.uni-due.de>
2020-11-23 11:49:10 +0100

    ButtonBuyProduct

--- SEPClient/src/main/java/SEPClient/MainScreenController.java
+++ SEPClient/src/main/java/SEPClient/MainScreenController.java
@@ -1,5 +1,14 @@
 package SEPClient;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MainScreenController {
+	
+	private Button MainScreen_ButtonSellProduct;
+	private Button MainScreen_ButtonBuyProduct;
+	
 
 }
