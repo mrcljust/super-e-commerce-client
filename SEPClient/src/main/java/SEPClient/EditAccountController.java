59bb06f3533bea0626bc705dbe981cc4cb5c50e1
Marcel Just <marcel.just@stud.uni-due.de>
2020-11-29 21:42:58 +0100

    Umlaute fix

--- SEPClient/src/main/java/SEPClient/EditAccountController.java
+++ SEPClient/src/main/java/SEPClient/EditAccountController.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Optional;
 
 import javax.imageio.ImageIO;
