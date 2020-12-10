3ca065bd66ff2769fce1573a8c240f7906f470c0
Marcel Just <marcel.just@stud.uni-due.de>
2020-12-11 00:41:14 +0100

    auction

--- SEPCommon/src/main/java/SEPCommon/Auction.java
+++ SEPCommon/src/main/java/SEPCommon/Auction.java
@@ -10,17 +10,18 @@ public class Auction implements Serializable {
 	private String description;
 	private byte[] image;
 	private double minBid;
-	private double maxBid;
+	private double currentBid;
 	private String shippingType;
 	private Customer seller;
-	private Customer maxBidder;
+	private Customer currentBidder;
 	private Rating sellerRating;
 	private Rating buyerRating;
-	private Date date;
+	private Date enddate;
 	private boolean isEnded;
 
-	public Auction(int _id, String _title, String _description, byte[] _image, double _minBid, String _shippingType,			//laufende Auktion aus DB holen	
-			Customer _seller) {
+	public Auction(int _id, String _title, String _description, byte[] _image, double _minBid, String _shippingType,
+			Customer _seller, Customer _currentBidder, Double _currentBid, Date _enddate) {
+		//wird aufgerufen, wenn laufende Auktion aus DB geholt wird
 		this.id = _id;
 		this.title = _title;
 		this.description = _description;
@@ -28,57 +29,58 @@ public class Auction implements Serializable {
 		this.minBid = _minBid;
 		this.shippingType = _shippingType;
 		this.seller = _seller;
-		this.isEnded=false;
-
+		this.isEnded = false;
+		this.enddate = _enddate;
+		this.currentBid = _currentBid;
+		this.currentBidder = _currentBidder;
 	}
 
-	public Auction(int _id, String _title, String _description, byte[] _image, double _minBid, double _maxBid,					//Dieser Konstruktor für gespeichert in DB
-			String _shippingType, Customer _seller, Customer _maxBidder, Rating _sellerRating, Rating _buyerRating,
-			Date _date) {
+	public Auction(int _id, String _title, String _description, byte[] _image, double _minBid, double _currentBid,
+			String _shippingType, Customer _seller, Customer _currentBidder, Rating _sellerRating, Rating _buyerRating,
+			Date _enddate) {
+		//wird bei beendeten Auktionen in der DB aufgerufen.
 		this.id = _id;
 		this.title = _title;
 		this.description = _description;
 		this.image = _image;
 		this.minBid = _minBid;
-		this.maxBid = _maxBid;
+		this.currentBid = _currentBid;
 		shippingType = _shippingType;
 		this.seller = _seller;
-		this.maxBidder = _maxBidder;
+		this.currentBidder = _currentBidder;
 		this.sellerRating = _sellerRating;
 		this.buyerRating = _buyerRating;
-		this.date = _date;
+		this.enddate = _enddate;
 		this.isEnded=true;
 	}
 
-	public Auction(String _title, String _description, byte[] _image, double _minBid, String _shippingType,			//neue Auktion anlegen
-			Customer _seller) {
+	public Auction(String _title, String _description, byte[] _image, double _minBid, String _shippingType,	
+			Customer _seller, Date _enddate) {
+		//wird aufgerufen, wenn neue Auktion angelegt wird (noch keine ID vergeben)
 		this.title = _title;
 		this.description = _description;
 		this.image = _image;
 		this.minBid = _minBid;
 		shippingType = _shippingType;
 		this.seller = _seller;
+		this.enddate = _enddate;
 		this.isEnded=false;
 	}
 
-	public Response SendBid(double bid, Customer bidder) {
-		return null;
+	public double getCurrentBid() {
+		return currentBid;
 	}
 
-	public double getMaxBid() {
-		return maxBid;
+	public void setCurrentBid(double _currentBid) {
+		this.currentBid = _currentBid;
 	}
 
-	public void setMaxBid(double maxBid) {
-		this.maxBid = maxBid;
+	public Customer getCurrentBidder() {
+		return currentBidder;
 	}
 
-	public Customer getMaxBidder() {
-		return maxBidder;
-	}
-
-	public void setMaxBidder(Customer maxBidder) {
-		this.maxBidder = maxBidder;
+	public void setCurrentBidder(Customer _currentBidder) {
+		this.currentBidder = _currentBidder;
 	}
 
 	public int getId() {
@@ -93,48 +95,48 @@ public class Auction implements Serializable {
 		return title;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
+	public void setTitle(String _title) {
+		this.title = _title;
 	}
 
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
+	public void setDescription(String _description) {
+		this.description = _description;
 	}
 
 	public byte[] getImage() {
 		return image;
 	}
 
-	public void setImage(byte[] image) {
-		this.image = image;
+	public void setImage(byte[] _image) {
+		this.image = _image;
 	}
 
 	public double getMinBid() {
 		return minBid;
 	}
 
-	public void setMinBid(double minBid) {
-		this.minBid = minBid;
+	public void setMinBid(double _minBid) {
+		this.minBid = _minBid;
 	}
 
 	public String getShippingType() {
 		return shippingType;
 	}
 
-	public void setShippingType(String shippingType) {
-		this.shippingType = shippingType;
+	public void setShippingType(String _shippingType) {
+		this.shippingType = _shippingType;
 	}
 
 	public Customer getSeller() {
 		return seller;
 	}
 
-	public void setSeller(Customer seller) {
-		this.seller = seller;
+	public void setSeller(Customer _seller) {
+		this.seller = _seller;
 	}
 
 }
