d91fc7ff03d01ea018827a918e0fad2d69fd3ba6
Yannis Bromby <yannis.bromby@stud.uni-due.de>
2020-12-22 18:34:42 +0100

    EmailHandler edits

--- SEPServer/src/main/java/SEPServer/EmailHandler.java
+++ SEPServer/src/main/java/SEPServer/EmailHandler.java
@@ -4,63 +4,72 @@ import SEPCommon.Auction;
 import SEPCommon.Response;
 import java.io.*;
 import java.net.InetAddress;
+import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Date;
-//import javax.mail.*;		
-//import javax.mail.internet.*;
-//import com.sun.mail.smtp.*;
+import javax.mail.*;		
+import javax.mail.internet.*;
+import com.sun.mail.smtp.*;
 
 public class EmailHandler {
 
 	
-	protected static Response sendAuctionEndedEmail(Auction auction) {
+	protected static Response sendAuctionEndedEmail(Auction auction)  {
 		//Verkaufsbestätigungsmail an den Verkäufer der Auktion schicken
 		
-//		Properties props = System.getProperties();
-//        props.put("mail.smtps.host","smtp.gmail.com");
-//        props.put("mail.smtps.auth","true");
-//        Session session = Session.getInstance(props, null);
-//        Message msg = new MimeMessage(session);
-//        msg.setFrom(new InternetAddress("mail@tovare.com"));;
-//        msg.setRecipients(Message.RecipientType.TO,
-//        InternetAddress.parse("tov.are.jacobsen@iss.no", false));
-//        msg.setSubject("Heisann "+System.currentTimeMillis());
-//        msg.setText("Med vennlig hilsennTov Are Jacobsen");
-//        msg.setHeader("X-Mailer", "Tov Are's program");
-//        msg.setSentDate(new Date());
-//        SMTPTransport t =
-//            (SMTPTransport)session.getTransport("smtps");
-//        t.connect("smtp.gmail.com", "admin@tovare.com", "<insert password here>");
-//        t.sendMessage(msg, msg.getAllRecipients());
-//        System.out.println("Response: " + t.getLastServerResponse());
-//        t.close();
+		try {
+		Properties props = System.getProperties();
+        props.put("mail.smtps.host","smtp.gmail.com");
+        props.put("mail.smtps.auth","true");
+        Session session = Session.getInstance(props, null);
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("sepgruppeb@gmail.com"));;
+        msg.setRecipients(Message.RecipientType.TO,
+        InternetAddress.parse("yannisbromby@gmx.de", false));
+        msg.setSubject("Auktion abgeschlossen "+System.currentTimeMillis());
+        msg.setText("Glückwunsch zur erfolgreichen Auktion. Das Produt wurde zu x $ an y verkauft.");
+        msg.setHeader("X-Mailer", "Auktion");
+        msg.setSentDate(new Date());
+        SMTPTransport t =
+            (SMTPTransport)session.getTransport("smtps");
+        t.connect("smtp.gmail.com", "sepgruppeb@gmail.com", "<wisem2020>");
+        t.sendMessage(msg, msg.getAllRecipients());
+        System.out.println("Response: " + t.getLastServerResponse());
+        t.close();
 		
-		return null;
+		return Response.Success;
+		} finally {
+			return null;
+		} 
 	}
 	
 	protected static Response sendAuctionEndedBuyerNoBalanceEmail(Auction auction) {
 		//Verkaufsbestätigungsmail an den Verkäufer der Auktion schicken
 		//folgender Spezialfall: User hat kein Guthaben mehr - entsprechende Mail verschicken
 		
-//		Properties props = System.getProperties();
-//        props.put("mail.smtps.host","smtp.gmail.com");
-//        props.put("mail.smtps.auth","true");
-//        Session session = Session.getInstance(props, null);
-//        Message msg = new MimeMessage(session);
-//        msg.setFrom(new InternetAddress("mail@tovare.com"));;
-//        msg.setRecipients(Message.RecipientType.TO,
-//        InternetAddress.parse("tov.are.jacobsen@iss.no", false));
-//        msg.setSubject("Heisann "+System.currentTimeMillis());
-//        msg.setText("Med vennlig hilsennTov Are Jacobsen");
-//        msg.setHeader("X-Mailer", "Tov Are's program");
-//        msg.setSentDate(new Date());
-//        SMTPTransport t =
-//            (SMTPTransport)session.getTransport("smtps");
-//        t.connect("smtp.gmail.com", "admin@tovare.com", "<insert password here>");
-//        t.sendMessage(msg, msg.getAllRecipients());
-//        System.out.println("Response: " + t.getLastServerResponse());
-//        t.close();
-		
-		return null;
-	}
+		try {
+			Properties props = System.getProperties();
+	        props.put("mail.smtps.host","smtp.gmail.com");
+	        props.put("mail.smtps.auth","true");
+	        Session session = Session.getInstance(props, null);
+	        Message msg = new MimeMessage(session);
+	        msg.setFrom(new InternetAddress("sepgruppeb@gmail.com"));;
+	        msg.setRecipients(Message.RecipientType.TO,
+	        InternetAddress.parse("yannisbromby@gmx.de", false));
+	        msg.setSubject("Auktion abgeschlossen "+System.currentTimeMillis());
+	        msg.setText("Glückwunsch zur erfolgreichen Auktion. Das Produt wurde zu x $ an y verkauft.");
+	        msg.setHeader("X-Mailer", "Auktion");
+	        msg.setSentDate(new Date());
+	        SMTPTransport t =
+	            (SMTPTransport)session.getTransport("smtps");
+	        t.connect("smtp.gmail.com", "sepgruppeb@gmail.com", "<wisem2020>");
+	      //  t.sendMessage(msg, msg.getAllRecipients());
+	        System.out.println("Response: " + t.getLastServerResponse());
+	        t.close();
+			
+			return Response.Success;
+			} finally {
+				return null;
+			} 
+		}
 }
