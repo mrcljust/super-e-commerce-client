e26243e3e52a4a246fe6a3673c253055e9bf2395
Hannah <hannah.kalker@stud.uni-due.de>
2021-01-26 17:24:16 +0100

    sendMessage

--- SEPClient/src/main/java/SEPClient/SendMessageController.java
+++ SEPClient/src/main/java/SEPClient/SendMessageController.java
@@ -3,10 +3,17 @@ package SEPClient;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
+import SEPCommon.ClientRequest;
+import SEPCommon.Message;
+import SEPCommon.Request;
+import SEPCommon.Response;
+import SEPCommon.ServerResponse;
 import SEPCommon.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -58,6 +65,47 @@ public class SendMessageController {
 
     @FXML
     void SendMessage_ButtonOK_Click(ActionEvent event) {
+    	
+    	String message = SendMessage_txtMessage.getText();
+    	
+    	if (message == null) {
+    		FXMLHandler.ShowMessageBox("Sie haben noch keine Nachricht verfasst.", "Fehler", "Fehler", AlertType.ERROR, true, false);			
+			return;
+    	}
+    	
+    	Message newMessage = new Message(sender, receiver, message);
+    	
+    	//unterscheidung ob buyer oder seller? ggf. noch abfangen
+    	
+    	HashMap <String, Object> requestMap = new HashMap<String, Object>();
+    	requestMap.put("Message", newMessage);
+    	
+    	
+    	
+    	ClientRequest req = new ClientRequest(Request.SendMessage, requestMap);
+    	Client client = Client.getClient();
+    	ServerResponse queryResponse = client.sendClientRequest(req);
+    	
+    	if(queryResponse.getResponseType() == Response.NoDBConnection) {
+			FXMLHandler.ShowMessageBox("Es konnte keine Verbindung zur Datenbank hergestellt werden, Ihre Nachricht wurde daher nicht abgeschickt.",
+					"Fehler", "Fehler", AlertType.ERROR, true, false);
+			return;
+		}
+
+    	else if (queryResponse.getResponseType() == Response.Failure) {
+    		FXMLHandler.ShowMessageBox("Es ist ein Fehler beim Verarbeiten Ihrer Anfrage aufgetreten, Ihre Nachricht wurde daher nicht abgeschickt.",
+					"Fehler", "Fehler", AlertType.ERROR, true, false);
+			return;
+		}
+    	
+    	else if (queryResponse.getResponseType() == Response.Success) {
+			FXMLHandler.ShowMessageBox("Ihre Nachricht wurde erfolgreich übermittelt.",
+					"Bewertung gespeichert", "Bewertung gespeichert", AlertType.INFORMATION, true, false);
+
+	        FXMLHandler.OpenSceneInStage((Stage) SendMessage_ButtonReturn.getScene().getWindow(), "MainScreen", "Super-E-commerce-Platform", true, true);
+
+    	
+    }
     	
     }
 
